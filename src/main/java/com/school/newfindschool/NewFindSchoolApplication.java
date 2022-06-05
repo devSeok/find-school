d65ba42548dcd850bfbdc15bd32bd25c5854ea3a
devSeok <stay0073@naver.com>
2022-06-05 16:56:28 +0900

    첫커밋

---
+++ src/main/java/com/school/newfindschool/NewFindSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.school.newfindschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFindSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewFindSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/school/newfindschool/NewFindSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.newfindschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewFindSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
